ae15c420c02b269ab669a94b282788cadf1f7af4
jestella <jestellaa@gmail.com>
2024-01-15 22:06:47 -0500

    add api key

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -4,12 +4,32 @@ import OpenAI from "openai";
 
 dotenv.config();
 
-// const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const config = new OpenAI({ apiKey: process.env.OPENAI_API_KEy });
+
+const openai = new OpenAI(config);
 
 const router = express.Router();
 
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Routes" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = response.data.data[0].b64_json;
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 });
 
 export default router;
