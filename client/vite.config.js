798820ddfb662ed8a105de95729ab2e3899af383
jestella <jestellaa@gmail.com>
2024-02-05 23:26:53 -0500

    delete server

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  chunkSizeWarningLimit: 1000,
   plugins: [react()],
   base: "/threejs-1/",
 });
